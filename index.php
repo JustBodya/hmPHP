21df267364602667176c57f32cf9f6e714c44da4
Bodya <bodyagame682005@gmail.com>
2023-04-23 21:01:10 +0700

    final work

--- index.php
+++ index.php
@@ -1,28 +1,38 @@
 <?php
 define('ROOT_DIR', $_SERVER['DOCUMENT_ROOT']);
 
-$value = 'The recording was successful';
+$value = [
+    'a' => [
+        'a',
+        'b',
+        'c'
+    ],
+    'b' => [
+        'd',
+        'e',
+        'f'
+    ]
+];
 $suf = "dir";
 
 _log($value, $suf);
 
-function _log($str, $suffix)
+function _log($str, $suffix = "logs")
 {
     counter();
+
     if (!is_dir(ROOT_DIR . "/logs" . "/$suffix")) {
         if (!is_dir("logs")) {
-            $mainPath = mkdir(ROOT_DIR . "/logs");
-            mkdir(ROOT_DIR . "/logs" . "/$suffix");
-        } else {
-            mkdir(ROOT_DIR . "/logs" . "/$suffix");
+            mkdir(ROOT_DIR . "/logs");
         }
+        mkdir(ROOT_DIR . "/logs" . "/$suffix");
     }
 
     if (is_array($str)) {
-        $str = json_encode($str, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+        $str = print_r($str, 1);
     }
 
-    $file = fopen(ROOT_DIR . "/logs//" . $suffix . "/logs.txt", "a+");
+    $file = fopen(ROOT_DIR . "/logs" . "/$suffix" . "/file.txt", "a+");
 
     fputs($file, date('Y-m-d H:i:s') . " $str" . "\n");
 }
